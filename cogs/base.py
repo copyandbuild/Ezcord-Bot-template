9a40e1565d53af3316fb9c2a2d93c288f6e8fd42
Larrox <payzitate@gmail.com>
2025-01-13 20:42:12 +0100

    added base cog

---
+++ cogs/base.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+
+class Base(commands.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+
+    @slash_command(description="hello")
+    async def hello(self, ctx: discord.ApplicationContext):
+        await ctx.respond(f"Hey {ctx.author.mention}!")
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Base(bot))
